0f1f2d7269dbc6039e2d97a83fd763d5813e072b
yachu <julie.gee1211@gmail.com>
2023-11-03 11:26:38 +0800

    Change hdf5 to pth of model file type

--- logisticregression.py
+++ logisticregression.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """
- @file   svm.py
- @brief  Ccode of SVM anomaly detection used experiment in [1].
- @author yachu,chi
+ @file   logisticregression.py
+ @author yachu,chi and minglun lee
  Copyright (C) 2019 Hitachi, Ltd. All right reserved.
  [1] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori Suefusa, and Yohei Kawaguchi, "MIMII Dataset: Sound Dataset for Malfunctioning Industrial Machine Investigation and Inspection," arXiv preprint arXiv:1909.09347, 2019.
 """
@@ -13,6 +12,7 @@ import pickle
 import os
 import sys
 import glob
+import torch
 ########################################################################
 
 
@@ -399,7 +399,7 @@ if __name__ == "__main__":
                                                                                        machine_type=machine_type,
                                                                                        machine_id=machine_id,
                                                                                        db=db)
-        model_file = "{model}/model_{machine_type}_{machine_id}_{db}.hdf5".format(model=param["model_directory"],
+        model_file = "{model}/supervisedmodel_logisticregression_{machine_type}_{machine_id}_{db}.pth".format(model=param["model_directory"],
                                                                                   machine_type=machine_type,
                                                                                   machine_id=machine_id,
                                                                                   db=db)
@@ -443,10 +443,8 @@ if __name__ == "__main__":
                                  random_state=0, solver='lbfgs', max_iter=100, 
                                  multi_class='auto', verbose=0, warm_start=False, 
                                  n_jobs=None, l1_ratio=None)
-        # if os.path.exists(model_file):
-        #     dc.load_weights(model_file)
-        # else:
         history = lg.fit(train_data, train_data_label)
+        torch.save(lg,model_file)
 
         # evaluation
         print("============== EVALUATION ==============")
