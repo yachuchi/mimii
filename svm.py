52726a19482fbe6b61a72f101881f6e3348fd31e
yachu <julie.gee1211@gmail.com>
2023-11-03 11:35:26 +0800

    Change hdf5 to pth of model file type

--- svm.py
+++ svm.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """
  @file   svm.py
- @brief  Ccode of SVM anomaly detection used experiment in [1].
- @author yachu,chi
+ @author yachu,chi and minglun lee
  Copyright (C) 2019 Hitachi, Ltd. All right reserved.
  [1] Harsh Purohit, Ryo Tanabe, Kenji Ichige, Takashi Endo, Yuki Nikaido, Kaori Suefusa, and Yohei Kawaguchi, "MIMII Dataset: Sound Dataset for Malfunctioning Industrial Machine Investigation and Inspection," arXiv preprint arXiv:1909.09347, 2019.
 """
@@ -13,6 +12,7 @@ import pickle
 import os
 import sys
 import glob
+import torch
 ########################################################################
 
 
@@ -402,7 +402,7 @@ if __name__ == "__main__":
                                                                                        machine_type=machine_type,
                                                                                        machine_id=machine_id,
                                                                                        db=db)
-        model_file = "{model}/model_{machine_type}_{machine_id}_{db}.hdf5".format(model=param["model_directory"],
+        model_file = "{model}/supervisedmodel_svm_{machine_type}_{machine_id}_{db}.pth".format(model=param["model_directory"],
                                                                                   machine_type=machine_type,
                                                                                   machine_id=machine_id,
                                                                                   db=db)
@@ -449,6 +449,7 @@ if __name__ == "__main__":
             lsvc.load_weights(model_file)
         else:
             history = lsvc.fit(train_data, train_data_label)
+        torch.save(lsvc,model_file)
 
         # evaluation
         print("============== EVALUATION ==============")
